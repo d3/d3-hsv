f5e4ab28051e7334b45178b5442e39183f6bc5b5
Mike Bostock <mbostock@gmail.com>
2016-06-08 16:15:11 -0700

    Get it working.

--- src/hsv.js
+++ src/hsv.js
@@ -1,19 +1,11 @@
-// import define, {extend} from "./define";
-// import {Color, rgbConvert, Rgb, darker, brighter} from "./color";
+import {color, rgb} from "d3-color";
 
-// TODO
-
-function define() {}
-function extend() {}
-function Color() {}
-function rgbConvert() {}
-function Rgb() {}
-function darker() {}
-function brighter() {}
+var darker = 0.7,
+    brighter = 1 / darker;
 
 function hsvConvert(o) {
   if (o instanceof Hsv) return new Hsv(o.h, o.s, o.v, o.opacity);
-  if (!(o instanceof Rgb)) o = rgbConvert(o);
+  if (!(o instanceof rgb)) o = rgb(o);
   var r = o.r / 255,
       g = o.g / 255,
       b = o.b / 255,
@@ -45,36 +37,41 @@ export function Hsv(h, s, v, opacity) {
   this.opacity = +opacity;
 }
 
-define(Hsv, hsv, extend(Color, {
-  brighter: function(k) {
-    k = k == null ? brighter : Math.pow(brighter, k);
-    return new Hsv(this.h, this.s, this.v * k, this.opacity);
-  },
-  darker: function(k) {
-    k = k == null ? darker : Math.pow(darker, k);
-    return new Hsv(this.h, this.s, this.v * k, this.opacity);
-  },
-  rgb: function() {
-    var h = isNaN(this.h) ? 0 : this.h % 360 + (this.h < 0) * 360,
-        s = isNaN(this.h) || isNaN(this.s) ? 0 : this.s,
-        v = this.v,
-        c = v * s,
-        x = c * (1 - Math.abs((h/60)%2 - 1)),
-        m = v - c;
-    if (h < 60) return hsv2rgb(c, x, 0, m, this.opacity);
-    else if (h < 120) return hsv2rgb(x, c, 0, m, this.opacity);
-    else if (h < 180) return hsv2rgb(0, c, x, m, this.opacity);
-    else if (h < 240) return hsv2rgb(0, x, c, m, this.opacity);
-    else if (h < 300) return hsv2rgb(x, 0, c, m, this.opacity);
-    return new hsv2rgb(c, 0, x, m, this.opacity);
-  },
-  displayable: function() {
-    return (0 <= this.s && this.s <= 1 || isNaN(this.s))
-        && (0 <= this.v && this.v <= 1)
-        && (0 <= this.opacity && this.opacity <= 1);
-  }
-}));
+var hsvPrototype = Hsv.prototype = hsv.prototype = Object.create(color.prototype);
+
+hsvPrototype.constructor = Hsv;
+
+hsvPrototype.brighter = function(k) {
+  k = k == null ? brighter : Math.pow(brighter, k);
+  return new Hsv(this.h, this.s, this.v * k, this.opacity);
+};
+
+hsvPrototype.darker = function(k) {
+  k = k == null ? darker : Math.pow(darker, k);
+  return new Hsv(this.h, this.s, this.v * k, this.opacity);
+};
+
+hsvPrototype.rgb = function() {
+  var h = isNaN(this.h) ? 0 : this.h % 360 + (this.h < 0) * 360,
+      s = isNaN(this.h) || isNaN(this.s) ? 0 : this.s,
+      v = this.v,
+      c = v * s,
+      x = c * (1 - Math.abs((h/60)%2 - 1)),
+      m = v - c;
+  if (h < 60) return hsv2rgb(c, x, 0, m, this.opacity);
+  else if (h < 120) return hsv2rgb(x, c, 0, m, this.opacity);
+  else if (h < 180) return hsv2rgb(0, c, x, m, this.opacity);
+  else if (h < 240) return hsv2rgb(0, x, c, m, this.opacity);
+  else if (h < 300) return hsv2rgb(x, 0, c, m, this.opacity);
+  return new hsv2rgb(c, 0, x, m, this.opacity);
+};
+
+hsvPrototype.displayable = function() {
+  return (0 <= this.s && this.s <= 1 || isNaN(this.s))
+      && (0 <= this.v && this.v <= 1)
+      && (0 <= this.opacity && this.opacity <= 1);
+};
 
 function hsv2rgb(r1, g1, b1, m, a) {
-  return new Rgb((r1+m) * 255, (g1+m)*255, (b1+m)*255, a);
+  return rgb((r1 + m) * 255, (g1 + m) * 255, (b1 + m) * 255, a);
 }
